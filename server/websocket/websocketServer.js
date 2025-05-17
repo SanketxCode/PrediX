faf332555e93239a585cb1171b11ff6501d0a3b3
Sanket Kumbhar <sanketkumbhar142@gmail.com>
2025-05-18 00:57:30 +0530

    added market-wise subscription support for real-time updates

--- server/websocket/websocketServer.js
+++ server/websocket/websocketServer.js
@@ -4,18 +4,44 @@ const supabase  =  require('../supaBaseClient');
 
 const wss = new WebSocket.Server({port:8080});
 
+const clients = new Map(); // client => subscribed market_ids will be added here
 wss.on('connection',(ws)=>{
     console.log('Client connected');
 
+
+    clients.set(ws,new Set());
+
+    
     ws.on('message',(message) =>{
         console.log('Recieved :',message);
-        
+
+        try{
+            const {action,market_id}  = JSON.parse(message);
+
+            console.log(action+" "+market_id)
+            if(action === 'subscribe' && market_id)
+            {
+                clients.get(ws).add(market_id);
+                ws.send(JSON.stringify({status:'subscribed',market_id}));
+            }
+            else if(action  === 'unsubscribe' && market_id)
+            {
+                clients.get(ws).delete(market_id);
+                ws.send(JSON.stringify({status:'unsubscribed',market_id}));
+            }
+            else
+            {
+                ws.send(JSON.stringify({error:'Invalid action  missing market_id '}));
+            }
+        }catch{
+            ws.send(JSON.stringify({error:'Invalid JSON'}));
+        }
     });
 
 
     ws.on('close',()=>{
         console.log('Client disconnected');
-    })
+    });
     
 });
 
